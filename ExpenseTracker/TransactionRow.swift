0d3b967bef6b87e23a97a2d584b3878fda7e5e52
KyleAnthonyHay <haykyle917@gmail.com>
2024-07-24 17:23:37 -0400

    transaction category added + fontawesome package added

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct TransactionRow: View {
     var transaction: Transaction
@@ -16,6 +17,9 @@ struct TransactionRow: View {
             RoundedRectangle(cornerRadius: 20, style: .continuous)
                 .fill(Color.Colors.icon.opacity(0.3))
                 .frame(width: 44, height: 44)
+                .overlay {
+                    FontIcon.text(.awesome5Solid(code: .icons), fontsize: 24, color: Color.Colors.icon)
+                }
             
             VStack(alignment: .leading, spacing: 6, content: {
                 //MARK: Transaction Merchant
