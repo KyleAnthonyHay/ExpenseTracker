159a97a164f3740081f9e5945438fed6607480a7
KyleAnthonyHay <haykyle917@gmail.com>
2024-07-24 16:58:21 -0400

    TRANSACTION ROW merchant, category, date and amount added

---
+++ ExpenseTracker/TransactionRow.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionRow.swift
+//  ExpenseTracker
+//
+//  Created by Kyle-Anthony Hay on 7/24/24.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionRow()
+}
