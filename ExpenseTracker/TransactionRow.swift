abcd487416740f889b3b1f07e31fe8ff8c02da9b
KyleAnthonyHay <haykyle917@gmail.com>
2024-07-24 17:18:26 -0400

    parced date added

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct TransactionRow: View {
+    var transaction: Transaction
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20, content: {
+            //MARK: Transaction Category Icon
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.Colors.icon.opacity(0.3))
+                .frame(width: 44, height: 44)
+            
+            VStack(alignment: .leading, spacing: 6, content: {
+                //MARK: Transaction Merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                //MARK: Transaction Category
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(7)
+                    .lineLimit(1)
+                //MARK: Transaction Date
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+            })//end of VStack
+            
+            Spacer()
+            //MARK: Transaction Amount
+            Text(transaction.signedAmount, format: .currency(code: "USD"))
+                .bold()
+                .foregroundStyle(transaction.type == TransactionType.credit.rawValue ? Color.text : .primary)
+        })
+        .padding([.top, .bottom], 8)
     }
 }
 
 #Preview {
-    TransactionRow()
+    TransactionRow(transaction: transactionPreviewData)
 }
